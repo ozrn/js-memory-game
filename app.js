31e04638d39340a62065304ae0c57015252dfc9f
ozrn <ozerenkevser92@gmail.com>
2022-08-28 12:44:20 +0300

    Check two clicked cards if they are matched

--- app.js
+++ app.js
@@ -78,6 +78,32 @@ const cardArray = [{
     }
   }
 
+  //check the two clicked cards if they are matched
+    function checkMatch() {
+      const cards = document.querySelectorAll('img')
+      const optionOneId = cardsChosenId[0]
+      console.log(optionOneId)
+      const optionTwoId = cardsChosenId[1]
+      console.log(optionTwoId)
+      console.log(cards)
+
+      if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/js-badge.svg')
+        cards[optionTwoId].setAttribute('src', 'images/js-badge.svg')
+      }
+      else if (cardsChosen[0] === cardsChosen[1]) {
+        cards[optionOneId].setAttribute('src', 'images/smiley.png')
+        cards[optionTwoId].setAttribute('src', 'images/smiley.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+      } else {
+        cards[optionOneId].setAttribute('src', 'images/js-badge.svg')
+        cards[optionTwoId].setAttribute('src', 'images/js-badge.svg')
+      }
+      cardsChosen = []
+      cardsChosenId = []
+    }
+
 
   //flip the card when clicked
     function flipCard() {
@@ -85,6 +111,9 @@ const cardArray = [{
       cardsChosen.push(cardArray[cardId].name)
       cardsChosenId.push(cardId)
       this.setAttribute('src', cardArray[cardId].img)
+      if (cardsChosen.length ===2 ) {
+          setTimeout(checkMatch, 500)
+        }
     }
 
 
